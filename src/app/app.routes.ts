1e869a3a6836e7579242df28aa9a9c39cbdaf4a3
Mario Hernandez <marioadonisjunior@gmail.com>
2024-05-29 11:55:00 -0500

    inicio page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { Receta1Component } from './pages/camisaNegraHombre/receta1.component';
 
 import { FormularioComponent } from './formulario/formulario.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-    {path:"", component: HomeComponent},
+    {path:"", component: MainComponent},
     {path:"receta1", component: Receta1Component},
     {path:"formulario", component: FormularioComponent},
   
