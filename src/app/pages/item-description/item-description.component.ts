ab2fd6822f4de0ffc9302d8e537cadc20a3a9565
amhernandez13 <amhernandez13@outlook.com>
2024-06-04 22:20:58 -0500

    Inicio del maquetado de item-description-

---
+++ src/app/pages/item-description/item-description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-description',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-description.component.html',
+  styleUrl: './item-description.component.css'
+})
+export class ItemDescriptionComponent {
+
+}
