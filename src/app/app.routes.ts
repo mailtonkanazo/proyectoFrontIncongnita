8a9586d74d0bd9c2c3c27782ac2edb4f43d153d3
Esteban Rico <estebitan952a@gmail.com>
2024-06-10 19:32:52 -0500

    Login routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
+
 import { Receta1Component } from './pages/camisaNegraHombre/receta1.component';
 
 import { FormularioComponent } from './formulario/formulario.component';
 import { MainComponent } from './pages/main/main.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ItemDescriptionComponent } from './pages/item-description/item-description.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:"", component: MainComponent},
+    {path:"login", component: LoginComponent},
     {path:"menu", component: MenuComponent},
     {path:"home", component: HomeComponent},
     {path:"receta1", component: Receta1Component},
