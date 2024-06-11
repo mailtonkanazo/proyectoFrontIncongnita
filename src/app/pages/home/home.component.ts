ca59214fe04789be3ed187dfddaaefebd93c9d44
Mario Hernandez <marioadonisjunior@gmail.com>
2024-06-11 18:34:40 -0500

    pagina de fachada o inicio main

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardProductsComponent } from '../../component/card-products/card-products.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardProductsComponent],
+  imports: [CardProductsComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
