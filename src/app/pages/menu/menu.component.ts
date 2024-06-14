522d8baed6ada36160241717f4a0fa19c29ef2f0
Mario Hernandez <marioadonisjunior@gmail.com>
2024-06-13 22:33:44 -0500

    arreglos menu

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  modeloMujer = false;
+  modeloHombre = false;
 
+  mostrarMujer() {
+    this.modeloMujer = true;
+    this.modeloHombre = false;
+  }
+
+  mostrarHombre() {
+    this.modeloMujer = false;
+    this.modeloHombre = true;
+  }
 }
