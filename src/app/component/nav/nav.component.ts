c93b901d516c9e9ad6b2c0f03b2c2379a93e37a9
amhernandez13 <amhernandez13@outlook.com>
2024-06-19 21:36:12 -0500

    se importó HttpClient en app.config.ts , y se comentaron varios elementos en el nav, que no permitian mostrar el nav correctamente

--- src/app/component/nav/nav.component.ts
+++ src/app/component/nav/nav.component.ts
@@ -1,13 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  menuVisible = false
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
   }
