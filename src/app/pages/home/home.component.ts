cef81a435673e6ee389a5f9f4dc9f7679edffdd3
amhernandez13 <amhernandez13@outlook.com>
2024-06-23 21:44:49 -0500

    ahora todos los productos pueden ser desplegados desde la base de datos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CardProductsComponent } from '../../component/card-products/card-products.component';
 import { FooterComponent } from '../../component/footer/footer.component';
 import { NavComponent } from '../../component/nav/nav.component';
@@ -17,15 +17,17 @@ export class HomeComponent {
   private productList = inject(ProductListService);
   private cartService = inject(CartService);
 
-  productCard = this.productList.productList;
+  productCard = this.productList.getProducts;
   productService: any;
-  products: any;
+  
+  products = signal<any>([]);
 
   ngOnInit() {
-    this.productService.list().subscribe({
-      next: (response: any) => {
-        this.products.set(response);
-        console.log(this.products);
+    console.warn('[ngOnInit] El componente Home ha sido inicializado');
+    this.productList.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+        console.log(products);
       },
       error: (error: any) => {
         console.log(error);
--- src/app/service/product-list.service.ts
+++ src/app/service/product-list.service.ts
@@ -1,72 +1,78 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductListService {
+  private http = inject(HttpClient);
 
   constructor() { }
 
-  productList = signal([
-    {
-      imagen: 'https://picsum.photos/300/400?image=10',
-      titulo: 'Camisa Blanca Hombre M/L',
-      precio: '50.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=20',
-      titulo: 'Camisa Negra Hombre S',
-      precio: '45.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=30',
-      titulo: 'Camisa Azul Hombre L',
-      precio: '55.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=40',
-      titulo: 'Camisa Roja Hombre XL',
-      precio: '60.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=50',
-      titulo: 'Camisa Verde Hombre M',
-      precio: '52.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=60',
-      titulo: 'Camisa Amarilla Hombre S',
-      precio: '48.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=70',
-      titulo: 'Camisa Naranja Hombre L',
-      precio: '53.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=80',
-      titulo: 'Camisa Violeta Hombre XL',
-      precio: '58.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=90',
-      titulo: 'Camisa Rosa Hombre M',
-      precio: '47.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=100',
-      titulo: 'Camisa Marrón Hombre S',
-      precio: '49.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=110',
-      titulo: 'Camisa Gris Hombre L',
-      precio: '51.000'
-    },
-    {
-      imagen: 'https://picsum.photos/300/400?image=120',
-      titulo: 'Camisa Azul Claro Hombre XL',
-      precio: '59.000'
-    }
-  ]);
+  getProducts() {
+    return this.http.get('http://localhost:3000/api/product');
+  }
+
+//   productList = signal([
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=10',
+//       titulo: 'Camisa Blanca Hombre M/L',
+//       precio: '50.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=20',
+//       titulo: 'Camisa Negra Hombre S',
+//       precio: '45.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=30',
+//       titulo: 'Camisa Azul Hombre L',
+//       precio: '55.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=40',
+//       titulo: 'Camisa Roja Hombre XL',
+//       precio: '60.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=50',
+//       titulo: 'Camisa Verde Hombre M',
+//       precio: '52.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=60',
+//       titulo: 'Camisa Amarilla Hombre S',
+//       precio: '48.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=70',
+//       titulo: 'Camisa Naranja Hombre L',
+//       precio: '53.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=80',
+//       titulo: 'Camisa Violeta Hombre XL',
+//       precio: '58.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=90',
+//       titulo: 'Camisa Rosa Hombre M',
+//       precio: '47.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=100',
+//       titulo: 'Camisa Marrón Hombre S',
+//       precio: '49.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=110',
+//       titulo: 'Camisa Gris Hombre L',
+//       precio: '51.000'
+//     },
+//     {
+//       imagen: 'https://picsum.photos/300/400?image=120',
+//       titulo: 'Camisa Azul Claro Hombre XL',
+//       precio: '59.000'
+//     }
+//   ]);
 }
