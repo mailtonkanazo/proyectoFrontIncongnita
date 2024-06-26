05cbaf253b1817afba2191bef21dccf4c70fdaab
Mario Hernandez <marioadonisjunior@gmail.com>
2024-06-25 20:24:33 -0500

    carrito para Item-description

--- src/app/pages/item-description/item-description.component.ts
+++ src/app/pages/item-description/item-description.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, inject } from '@angular/core';
 import { NavComponent } from '../../component/nav/nav.component';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { CartService } from '../../service/cart.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-item-description',
   standalone: true,
   imports: [NavComponent, FooterComponent],
   templateUrl: './item-description.component.html',
-  styleUrl: './item-description.component.css'
+  styleUrl: './item-description.component.css',
 })
 export class ItemDescriptionComponent {
+  producto: any;
 
+  @Input() product: any;
+  private cartService = inject(CartService);
+  productQuantity = new FormControl(0);
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['product'] && this.product) {
+      this.productQuantity.setValue(this.product.quantity);
+    }
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
+
+  increment(productId: string) {
+    this.cartService.incrementQuantity(productId);
+  }
+
+  decrement(productId: string) {
+    this.cartService.decrementQuantity(productId);
+  }
+
+  deleteItem(productId: string) {
+    this.cartService.deleteItem(productId);
+  }
 }
